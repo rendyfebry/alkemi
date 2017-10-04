e8f0daba3086470d38fd883eb5457391d3a92241
Rendy Febry <rendyfebry@gmail.com>
2017-10-04 22:23:19 +0700

    Lint COde

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,10 @@
-const express = require("express")
+const express = require('express')
 
 const router = express.Router()
 
 /* GET home page. */
-router.get("/", (req, res) => {
-	res.render("index", { title: "Alkemi" })
+router.get('/', (req, res) => {
+	res.render('index', { title: 'Alkemi' })
 })
 
 module.exports = router

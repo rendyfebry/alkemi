8c5b0b74f1e48472c0056253cf5eb37a6fe57d99
Rendy Febry <rendyfebry@gmail.com>
2017-10-04 22:23:10 +0700

    Restructure Folders

---
+++ src/assets/pages/Homepage.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+import 'bootstrap/dist/css/bootstrap.css'
+
+import styles from './Homepage.sass'
+
+
+class Homepage extends Component {
+	render() {
+		return (
+			<div className={styles.wrapper}>
+				Home
+			</div>
+		)
+	}
+}
+
+export default Homepage
